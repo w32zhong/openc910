91ee5383e2bc91e5184832662113d8e1909e03b0
t-k- <clock126@126.com>
2022-05-15 18:18:41 -0400

    run cpu verilator with toolchain.

--- smart_run/build.sh
+++ smart_run/build.sh
@@ -1,10 +1,12 @@
 # See https://zhuanlan.zhihu.com/p/430738271
 
 export CODE_BASE_PATH=$(readlink -f ../C910_RTL_FACTORY)
-export TOOL_EXTENSION=/opt/toolchain/riscv64/bin
 #make help
 mkdir -p ./work
 make cleanVerilator
 make compile SIM=verilator
 make buildVerilator
+
+export TOOL_EXTENSION=$(readlink -f ../../xuantie-toolchain/Xuantie-900-gcc-elf-newlib-x86_64-V2.0.3/bin)
+(cd ./work/obj_dir && make -f Vtop.mk)
 make runcase CASE=hello_world SIM=verilator
