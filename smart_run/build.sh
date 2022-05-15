f6c37b3e96a86196aa50242a7059d071c6c65936
t-k- <clock126@126.com>
2022-05-15 14:59:47 -0400

    fix building.

--- smart_run/build.sh
+++ smart_run/build.sh
@@ -1,5 +1,10 @@
+# See https://zhuanlan.zhihu.com/p/430738271
+
 export CODE_BASE_PATH=$(readlink -f ../C910_RTL_FACTORY)
 export TOOL_EXTENSION=/opt/toolchain/riscv64/bin
 #make help
-make compile
+mkdir -p ./work
+make cleanVerilator
+make compile SIM=verilator
+make buildVerilator
 make runcase CASE=hello_world SIM=verilator
